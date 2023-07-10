b4e71a339d497ed8ae9e806f4dc7dfdd509099de
Phan hoang vinh hien <70145901+hien-p@users.noreply.github.com>
2023-07-10 20:02:12 +0700

    Update setup.py

--- botcore/setup.py
+++ botcore/setup.py
@@ -10,7 +10,7 @@ def load_my_env():
 
 def enable_tracing(session:str='test-deploy') -> bool:
     load_my_env()
-    lang_key = os.getenv("LANGCHAIN")
+    lang_key = st.secrets['LANGCHAIN']
     os.environ["LANGCHAIN_TRACING_V2"] = "true"
     os.environ["LANGCHAIN_ENDPOINT"] = "https://api.langchain.plus"
     os.environ["LANGCHAIN_API_KEY"] = lang_key
@@ -20,9 +20,15 @@ def enable_tracing(session:str='test-deploy') -> bool:
 
 def trace_palm2(model_name:str = 'text-bison', max_tokens:int = 280, session:str="test-deploy") -> VertexAI:
     enable_tracing(session)
-    service_json_path = f'{os.path.dirname(__file__)}/../.google/service_account.json'
+    #service_json_path = f'{os.path.dirname(__file__)}/../.google/service_account.json'
+    service_json_path = f"{os.path.dirname(__file__)}/service.json"
     os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = service_json_path
     model = VertexAI(model_name=model_name, max_output_tokens=max_tokens,verbose=True, temperature=0.0)
     print("Vertex AI Palm 2 ready")
     return model
+    
+def build_service(data_str):
+    data = json.loads(data_str)
+    with open(f'{os.path.dirname(__file__)}/service.json', 'w') as f:
+        json.dump(data, f)
 
